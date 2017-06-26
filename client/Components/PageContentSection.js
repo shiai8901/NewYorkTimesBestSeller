aacb8a32ee0dd8527566ffc512b33e07ea5220f7
Ai Shi <shiai8901@gmail.com>
2017-06-26 14:13:47 -0700

    added list name and pass book list to BookMenu component

---
+++ client/Components/PageContentSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookMenu from './BookMenu';
+
+export default class PageContentSection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			books: [],
+			display_name: "",
+			list_id: 0,
+			list_image: "",
+			list_image_height: 0,
+			list_image_width: 0,
+			list_name: "",
+			list_name_encoded: "",
+			updated: ""
+		}
+	}	
+
+	componentWillMount() {
+		
+			this.setState({
+				books: this.props.list.books,
+				display_name: this.props.list.display_name,
+				list_id: this.props.list.list_id,
+				list_image: this.props.list.list_image,
+				list_image_height: this.props.list.list_image_height,
+				list_image_width: this.props.list.list_image_width,
+				list_name: this.props.list.list_name,
+				list_name_encoded: this.props.list.list_name_encoded,
+				updated: this.props.list.updated				
+			});
+		
+	}
+
+	render() {
+		console.log("PageContentSection: ", this.props);
+
+		return (<div className="page_content_section">
+			<h3 className="page_content_section_listName">{this.props.list.list_name}</h3>
+			<BookMenu className="book_menu" 
+				books={this.props.list.books} />
+			</div>)
+	}
+}
