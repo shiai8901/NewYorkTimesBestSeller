976eda6d895e25320290dbb2d15b1207c6aeef37
Ai Shi <shiai8901@gmail.com>
2017-06-27 11:28:16 -0700

    pass encoded list name to component and render it

--- client/Components/PageHeader.js
+++ client/Components/PageHeader.js
@@ -9,8 +9,8 @@ export default class PageHeader extends React.Component {
 
 	render() {
 		return (<div className="page_header">
-			<h1 className="page_heading">page heading</h1>
-			<h2 className="page_subheading">page subheading</h2>
+			<h1 className="page_heading">{this.props.page_heading}</h1>
+			<h2 className="page_subheading">{this.props.page_subheading}</h2>
 			</div>)
 	}
 }
