f349df917ea6bdde9b1f558f6bebc84f5c069d32
Ai Shi <shiai8901@gmail.com>
2017-06-29 14:28:56 -0700

    setup default image

--- client/Components/BookMenuItem.js
+++ client/Components/BookMenuItem.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 
 export default class BookMenuItem extends React.Component {
 
+				// <img className="book_image thumbnail" src={this.props.book.book_image} ></img>
 	render() {
 		let context = this;
-		let style = {
-				height: context.props.book.book_image_height,
-				width: context.props.book.book_image_width 
-			}
+
 
 		if (this.props.page_heading && this.props.page_heading !== "The New York Times Best Sellers" ) {
 			return (<div className="book_menu_item detai_view">
-			<p className="rank">{this.props.book.rank}</p>
+			<p className="rank">{this.props.rank}</p>
 			<article className="book">
-				<img className="book_image thumbnail" src={this.props.book.book_image} ></img>
+				<img className="book_image thumbnail" src={this.props.book.book_image || "https://s1.nyt.com/du/books/images/default-image.png"} ></img>
 				<div className="book_body">
 				<p className="fressness">{this.props.book.weeks_on_list} weeks on the list</p>
 				<p className="title">{this.props.book.title}</p>
@@ -24,12 +22,17 @@ export default class BookMenuItem extends React.Component {
 			</article>
 			</div>)
 
-		} 
+		} else {
+			let style = {
+				height: context.props.book.book_image_height,
+				width: context.props.book.book_image_width 
+			}
+		}
 
 		return (<div className="book_menu_item">
 			<p className="rank">{this.props.book.rank}</p>
 			<article className="book">
-				<img className="book_image thumbnail" src={this.props.book.book_image} ></img>
+				<img className="book_image thumbnail" src={this.props.book.book_image || "https://s1.nyt.com/du/books/images/default-image.png"} ></img>
 				<div className="book_body">
 				<p className="fressness">{this.props.book.weeks_on_list} weeks on the list</p>
 				<p className="title">{this.props.book.title}</p>
